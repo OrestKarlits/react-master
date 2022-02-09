4381223e1f9e21d302ace59936955d0be670b03e
OrestKarlits <orestkarlits@gmail.com>
2022-02-09 15:19:29 +0200

    cw-2 with button

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -6,14 +6,20 @@ const Forms = ({getfilter}) => {
     const formHandler = (e) => {
     const eventdata = {...form, [e.target.name]:e.target.value}
         setform({...form, ...eventdata})
-        getfilter(eventdata)
     }
+
+    const onsubmit = (e) => {
+    e.preventDefault()
+        getfilter(form)
+    }
+
     return (
         <div>
         <form>
             <label>Name: <input type="text" name={'name'} value={form.name} onChange={formHandler} /></label>
             <label>UserName: <input type="text" name={'username'} value={form.username} onChange={formHandler} /></label>
             <label>Email: <input type="text" name={'email'} value={form.email} onChange={formHandler} /></label>
+            <button onClick={onsubmit}>Filter</button>
         </form>
 
         </div>
